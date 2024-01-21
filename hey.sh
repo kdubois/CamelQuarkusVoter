3bd73c82aa5aa9a5f2640aa18a89823de257aba1
Kevin Dubois <kevin.dubois@gmail.com>
2024-01-21 16:47:34 +0100

    update quarkus to 3.6.6

--- hey.sh
+++ hey.sh
@@ -1,2 +1,2 @@
-URL=$(kn service describe cameldemo-ingester -o url)
+URL=$(kn service describe cameldemo-ui -o url)
 hey -n 2000 -c 200 -H "Content-Type: application/json" -m POST -d "{\"shortname\":\"openshiftdevspaces\"}" ${URL}/favstack
