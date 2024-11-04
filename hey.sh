a7cb1ae1e0981f9c66bf101a4620e19114a0e218
Kevin Dubois <kevin.dubois@gmail.com>
2024-11-04 14:29:11 +0100

    add kube files for serverless and strimzi

--- hey.sh
+++ hey.sh
@@ -1,3 +1,18 @@
 URL=$(kn service describe cameldemo-ingester -o url)
 #URL=http://localhost:8081
-hey -n 1000 -c 200 -H "Content-Type: application/json" -m POST -d "{\"shortname\":\"openshiftdevspaces\"}" ${URL}/favstack
+
+
+if [ -z $1 ]; then
+    max=1
+else
+    max=$1
+fi
+
+x=1
+
+while [ $x -le $max ]
+do
+    hey -n 1000 -c 200 -H "Content-Type: application/json" -m POST -d "{\"shortname\":\"openshiftdevspaces\"}" ${URL}/favstack
+    x=$((x+1))
+done;
+
